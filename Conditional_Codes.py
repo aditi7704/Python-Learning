7b18aff788d2f872d6448bb67f72c85ef5d4eda8
Aditi Neharkar <neharkaraditi77@gmail.com>
2025-12-06 09:24:56 +0530

    Update Conditional_Codes.py

--- Conditional_Codes.py
+++ Conditional_Codes.py
@@ -6,14 +6,14 @@ elif n < 0:
     print("Negative")
 else:
     print("Zero")
-Zero
+
 #2. Check whether a number is **even or odd**.
 n=int(input("Enter Number:"))
 if n % 2 == 0:
       print("Even")
 else:
     print("Odd")
-Odd
+
 #3. Check if a given number is **divisible by 5**.
 n=int(input("Enter Number:"))
 if n % 5 == 0:
@@ -27,14 +27,14 @@ if n % 3 == 0 and n % 7 == 0:
     print("Yes")
 else:
     print("No")
-Yes
+
 #5. Write a program to check whether a given year is a **leap year**.
 year=int(input("Enter Leap Year:"))
 if year % 4 == 0 and year % 100 !=0:
     print("Leap year")
 else:
     print("Not Leap Year")
-Leap year
+
 #6. Check whether a given character is a **vowel or consonant**.
 ch=input("Enter Character:")
 v=['a','e','i','o','u']
@@ -42,4 +42,4 @@ if ch in :
     print("Vowels")
 else:
     print("consonent")
-consonent
+
