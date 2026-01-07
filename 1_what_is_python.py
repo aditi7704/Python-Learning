f44822da61f7b6381e5a294a482b7cfc6fd25691
Aditi Neharkar <neharkaraditi77@gmail.com>
2026-01-07 20:01:05 +0530

    Create 1_what_is_python.py

---
+++ 1_what_is_python.py
@@ -0,0 +1,7 @@
+Python is a high level , general purpose programming langauge known for its syntax making it easy to learn and read.
+It was created by Guido Van Rossum in 1991.
+It is used for :
+  web development
+  software development
+  mathmatics 
+  system scripting
