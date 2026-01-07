67895cadd74cae243a58bfc3a521e37d5fd52e4e
Aditi Neharkar <neharkaraditi77@gmail.com>
2026-01-07 20:06:57 +0530

    Add introductory points about Python

---
+++ 2_why_python.py
@@ -0,0 +1,5 @@
+Python works on different platforms (windows,mac,linux,Raspberry Pi , etc.,)
+Python has similar syntax to the english langauge.
+Python has syntax allows developers to write programs in some fewer lines than some other programming langauges.
+Python runs on an interpreter system means the code will executed as soon as it is written.This means prototyping can be very quick.
+Python can be treated in a object-oriented way , functional-way , procedural-way.
