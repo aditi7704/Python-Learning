3a6e996d4420fe4b6b02193ba48afce497e2cadd
Aditi Neharkar <neharkaraditi77@gmail.com>
2026-01-07 20:11:04 +0530

    Create 3_what_can_python_do.py

---
+++ 3_what_can_python_do.py
@@ -0,0 +1,5 @@
+Python can be used on a server to create web applications.
+Pyhton can be used alongside software to create workflows.
+Python can connect to database systems. It can also read and modify files.
+Python can be used to handle big data and complex mathmatics.
+Python can be used for rapid-prototyping and production ready software development.
