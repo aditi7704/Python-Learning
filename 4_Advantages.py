0726985fd6117c7ac43c9f4b91813fafbe5d9482
Aditi Neharkar <neharkaraditi77@gmail.com>
2026-01-07 20:18:37 +0530

    Create 4_Advantages.py

---
+++ 4_Advantages.py
@@ -0,0 +1,46 @@
+1.    Simple and easy to learn:
+Python has a clean and readable syntax.
+Looks like English, so beginners learn it quick. 
+Reduces development time.
+
+2.    Interpreted Language
+Code runs line-by-line.
+Easier to debug since errors are detected line-wise.
+No separate compilation step needed.
+
+3.   High-Level Langauge:
+You don’t need to manage memory manually.
+Python handles complex operations internal
+
+4. Object-oriented programming
+Supports classes, objects, inheritance, polymorphism, encapsulation.
+Helps in building reusable and modular co.
+
+5. Dynamically Typed
+No need to declare variable types.
+Variable type is decided at runti.## 
+Makes coding faster and more flee.
+
+6. Platform Independent:
+
+7. Large Standard Library:
+  
+8. Huge Community support:
+  
+9. Extensive Third Party Libraries
+Millions of developers use Python worldwide.
+Quick help available through forums, documentation, and tutoria.
+
+10. Sustaianable for almost every domain:
+Python is used in:
+Artificial Intelligence Machine Learning & Deep Learning Data Science Web apps Automation/Scripting Cybersecurity Game development IoT Cloud computing
+nal libraries.
+
+11. Strong Integration Properties:
+Works well with C, C++, Java, .NET.
+Good for embedded systems and enterprise solutions.
+
+12. Rapid Development
+Because of simplicity + huge libraries.
+Ideal for startups, prototyping, and researh.
+  
