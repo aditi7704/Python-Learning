94e723d8cb10afd3ed16e4747c3446ecf930cdb9
Aditi Neharkar <neharkaraditi77@gmail.com>
2026-01-07 20:19:45 +0530

    Add list of disadvantages for the technology

---
+++ 5_Disadvantages.py
@@ -0,0 +1,8 @@
+1. Slow speed
+2. High Memory Usage
+3. Not suitable for mobile development
+4. Weak in brower / Frontend development
+5. Runtime errors
+6. slower for mobile or gaming
+7. Database access is slower
+8. Packaging and deployement can be complex
